6d0477568b42ff9c88cb68b8d434a7093e02e80f
zulfadhli13 <jamaluddinzulfadhli13@gmail.com>
2023-11-20 09:22:13 +0700

    Pembaharuan

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed('.typing', {
-    strings: ['Blogger', 'UI/UX Designer', 'Front-End Developer '],
+    strings: ['UI/UX Designer', 'Front End Developer '],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed('.typing-2', {
-    strings: ['Blogger', 'UI/UX Designer', 'Front-End Developer '],
+    strings: ['UI/UX Designer', 'Front End Developer '],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
